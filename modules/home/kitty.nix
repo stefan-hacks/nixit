480f12bd03101d79962c6c30f5716916936e84ae
Lin <lin@serveme.lan>
2026-07-28 03:01:21 -0300

    fix kitty: ensure ~/.config/kitty is writable before HM links files

--- modules/home/kitty.nix
+++ modules/home/kitty.nix
@@ -1,6 +1,15 @@
 { config, pkgs, lib, ... }:
 
 {
+  # Ensure ~/.config/kitty exists and is writable by the user before HM links files
+  home.activation.ensureKittyDir = lib.hm.dag.entryBefore [ "writeBoundary" ] ''
+    if [ -d "$HOME/.config/kitty" ]; then
+      chmod u+rwx "$HOME/.config/kitty"
+    else
+      mkdir -p "$HOME/.config/kitty"
+    fi
+  '';
+
   home.file.".config/kitty/kitty.conf".source = ../../dotfiles/kitty/kitty.conf;
   home.file.".config/kitty/current-theme.conf".source = ../../dotfiles/kitty/current-theme.conf;
 }
