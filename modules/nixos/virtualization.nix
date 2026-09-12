1f9bc6dad80d9a2002e0b8aced248f7afe671035
Stefan <stefan-hacks@users.noreply.github.com>
2026-09-12 11:57:08 -0300

    nixos/virt: auto-start libvirt default network on boot

--- modules/nixos/virtualization.nix
+++ modules/nixos/virtualization.nix
@@ -17,4 +17,17 @@
     # virtualbox.host.enable = false;
     # virtualbox.host.enableExtensionPack = false;
   };
+
+  # Auto-start the default NAT network after libvirtd comes up
+  systemd.services.libvirt-default-network = {
+    description = "Auto-start libvirt default network";
+    after = [ "libvirtd.service" ];
+    requires = [ "libvirtd.service" ];
+    wantedBy = [ "multi-user.target" ];
+    serviceConfig = {
+      Type = "oneshot";
+      RemainAfterExit = "yes";
+      ExecStart = "${pkgs.libvirt}/bin/virsh net-start default || true";
+    };
+  };
 }
